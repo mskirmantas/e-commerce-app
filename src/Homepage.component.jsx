f051e032a190bcf9e990e1e4c83f790144c09640
Mantas Skirmantas <mantas.skirmantas@yahoo.com>
2021-05-11 10:34:35 +0300

    Initial commit

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import './Homepage.styles.scss';
+
+const Homepage = () => (
+
+    <div className="HomePage">
+        <div className="directory-menu">
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">BELTS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JEANS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">CAPS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+)
+
+export default Homepage;
